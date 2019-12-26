083df5a2d11ffb6c9a46b2facb33c31890831d94
chiendoan <chiendv311@gmail.com>
2019-12-26 13:54:36 +0700

    initial commit

---
+++ src/main/java/com/example/demoChienDVApp/DemoChienDvAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoChienDVApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoChienDvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoChienDvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoChienDVApp/DemoChienDvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoChienDVApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoChienDvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
